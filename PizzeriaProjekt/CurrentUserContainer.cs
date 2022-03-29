39860dff2f346a128519e0fe526fffbcd782a689
Jakub WIlk <jakub_wilk@outlook.com>
2022-03-29 21:43:48 +0200

    Continued working on project structure by adding mathods for logging in a user. Had to create a new migration for changed User Name to Login

---
+++ PizzeriaProjekt/CurrentUserContainer.cs
@@ -0,0 +1,14 @@
+﻿using PizzeriaProjekt.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaProjekt
+{
+    internal class CurrentUserContainer
+    {
+        public static User currentUser;
+    }
+}

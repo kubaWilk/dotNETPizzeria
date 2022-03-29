3d93cb4380de2cbad2bf7aba2efd9e01ebf66fdb
wsapon <sapon.wojciech@outlook.com>
2022-03-29 22:27:05 +0200

    Hid password string in textbox, added login button

--- PizzeriaProjekt/Form1.cs
+++ PizzeriaProjekt/Form1.cs
@@ -1,39 +1,68 @@
+using PizzeriaProjekt.Exceptions;
+using PizzeriaProjekt.Service;
 using System.Windows.Forms;
 namespace PizzeriaProjekt
 {
-    public partial class Form1 : Form
-    {
+    
+        
+        public partial class Form1 : Form
+        {
+
+        UserService userService = new UserService();  
         public Form1()
-        {
-            InitializeComponent();
-           
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            textBox1.Text = "Login";
-            textBox2.Text = "Has³o";
-        }
-
-        private void textBox1_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "Login")
             {
-                textBox1.Clear();
+                InitializeComponent();
+            textBox2.PasswordChar = '*';
+            textBox1.MaxLength = 32;
+
             }
-            else
-                return;
-        }
-
-        private void textBox2_Click(object sender, EventArgs e)
-        {
-
-            if (textBox2.Text == "Has³o")
+            private void Form1_Load(object sender, EventArgs e)
             {
-                textBox2.Clear();
+                textBox1.Text = "Login";
+                textBox2.Text = "Has³o";
             }
-            else
-                return;
+
+            private void textBox1_Click(object sender, EventArgs e)
+            {
+                if (textBox1.Text == "Login")
+                {
+                    textBox1.Clear();
+                }
+                else
+                    return;
+            }
+
+            private void textBox2_Click(object sender, EventArgs e)
+            {
+
+                if (textBox2.Text == "Has³o") 
+                {
+                    textBox2.Clear();
+                }
+                else
+                    return;
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+             try
+            {
+                if (userService.LogIn("test" ,"test")) {//zmieniæ na odniesienie do DB!
+                    MessageBox.Show($"Zapraszamy! {CurrentUserContainer.currentUser.Id} {CurrentUserContainer.currentUser.Login} {CurrentUserContainer.currentUser.Password} ");
+                }
+                else
+                {
+                    MessageBox.Show("Nieprawid³owe has³o");
+                    textBox2.Clear();
+                }
+                
+            }
+            catch (UserNotFoundException)
+            {
+                MessageBox.Show("Niepoprawny login");
+            }
+            }
         }
-    }
+
     
 }

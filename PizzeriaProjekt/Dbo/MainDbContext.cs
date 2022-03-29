fd8b19a68dcdec765eaaec3c116c4acfc4de201a
Jakub WIlk <jakub_wilk@outlook.com>
2022-03-29 20:17:57 +0200

    Initial Project structure + DB connection

---
+++ PizzeriaProjekt/Dbo/MainDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzeriaProjekt.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaProjekt.Dbo
+{
+    internal class MainDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = @"Server=127.0.0.1;Port=3306;Database=pizzaDB;Uid=root;Pwd=root;";
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    }
+}

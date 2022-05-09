7d98c673de421913f7dfd4addfb9bbcc5278060f
wsapon <sapon.wojciech@outlook.com>
2022-05-09 22:10:01 +0200

    set visibility of popups to default false

--- PizzeriaProjekt/GUI/RegistrationWindow/rulesPage.xaml.cs
+++ PizzeriaProjekt/GUI/RegistrationWindow/rulesPage.xaml.cs
@@ -41,8 +41,9 @@ namespace PizzeriaProjekt
             }
             catch (System.IO.DirectoryNotFoundException)
             {
-                testbox.Text = "błąd odczytu pliku";
                 Accepted.IsEnabled = true;
+                testbox.Text = "błąd odczytu pliku";
+               
             }
 
             Accepted.IsEnabled = false;

c6c5698b8291fb3a66eaddddf9f3f4c5f2a9c805
Jakub WIlk <jakub_wilk@outlook.com>
2022-05-12 23:11:49 +0200

    moved adding order items to another method

--- PizzeriaProjekt/GUI/MainMenu/OrderHistory/selectedOrderPage.xaml.cs
+++ PizzeriaProjekt/GUI/MainMenu/OrderHistory/selectedOrderPage.xaml.cs
@@ -28,35 +28,12 @@ namespace PizzeriaProjekt.GUI.MainMenu.OrderHistory
         {
             InitializeComponent();
             this.pizzaOrder = pizzaOrder;
-            initializeOrderDetails();
 
-            foreach(PizzaOrder.Item item in pizzaOrder.Items)
-            {
-                TreeViewItem root = new TreeViewItem() { Header = $"{item.Pizza.Name}" };
-
-                root.Items.Add(new TreeViewItem() { Header = $"Rodzaj Ciasta: {item.Crust.Name} ({item.Crust.BasePrice}zł)" });
-                root.Items.Add(new TreeViewItem() { Header = $"Rozmiar: {item.Size.Name} ({item.Size.BasePrice}zł)" });
-
-                TreeViewItem toppingsNode = new TreeViewItem() { Header = "Składniki" };
-
-                foreach(PizzaTopping topping in item.Pizza.PizzaToppings)
-                {
-                    toppingsNode.Items.Add(new TreeViewItem() { Header = $"{topping.Topping.Name} {topping.Topping.BasePrice}zł" });
-                }
-
-                foreach(Topping topping in item.ExtraToppings)
-                {
-                    toppingsNode.Items.Add(new TreeViewItem() { Header = $"{topping.Name} ({topping.BasePrice}zł)" });
-                }
-
-                root.Items.Add(toppingsNode);
-                root.Items.Add(new TreeViewItem() { Header = $"Łączna cena: ({item.ActualPrice}zł)" });
-
-                orderItemsTreeView.Items.Add(root);
-            }
+            setCustomerData();
+            setOrderItems();
         }   
 
-        private void initializeOrderDetails()
+        private void setCustomerData()
         {
             titleLabel.Content = $"ZAMÓWIENIE NR {pizzaOrder.OrderId}";
             whoLabel.Content += $" {pizzaOrder.User.FirstName} {pizzaOrder.User.LastName}";
@@ -75,6 +52,34 @@ namespace PizzeriaProjekt.GUI.MainMenu.OrderHistory
                 statusLabel.Content += " w realizacji";
             }
             summaryLabel.Content += $"{pizzaOrder.ActualPrice}zł";
+        }
+
+        private void setOrderItems()
+        {
+            foreach (PizzaOrder.Item item in pizzaOrder.Items)
+            {
+                TreeViewItem root = new TreeViewItem() { Header = $"{item.Pizza.Name}" };
+
+                root.Items.Add(new TreeViewItem() { Header = $"Rodzaj Ciasta: {item.Crust.Name} ({item.Crust.BasePrice}zł)" });
+                root.Items.Add(new TreeViewItem() { Header = $"Rozmiar: {item.Size.Name} ({item.Size.BasePrice}zł)" });
+
+                TreeViewItem toppingsNode = new TreeViewItem() { Header = "Składniki" };
+
+                foreach (PizzaTopping topping in item.Pizza.PizzaToppings)
+                {
+                    toppingsNode.Items.Add(new TreeViewItem() { Header = $"{topping.Topping.Name} {topping.Topping.BasePrice}zł" });
+                }
+
+                foreach (Topping topping in item.ExtraToppings)
+                {
+                    toppingsNode.Items.Add(new TreeViewItem() { Header = $"{topping.Name} ({topping.BasePrice}zł)" });
+                }
+
+                root.Items.Add(toppingsNode);
+                root.Items.Add(new TreeViewItem() { Header = $"Łączna cena: ({item.ActualPrice}zł)" });
+
+                orderItemsTreeView.Items.Add(root);
+            }
         }
 
         private void backButton_Click(object sender, RoutedEventArgs e)

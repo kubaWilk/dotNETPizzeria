3ab909bfbd8f06cf5e3cae5a1dc14b828f10dbc6
wsapon <sapon.wojciech@outlook.com>
2022-06-09 22:00:29 +0200

    added Query test

---
+++ PizzeriaServerTests/QueryTests/PizzaQueryDaoMock.cs
@@ -0,0 +1,90 @@
+﻿using PizzeriaServer.Meals.Dal;
+using PizzeriaServer.Meals.Exceptions;
+using PizzeriaServer.Meals.Models;
+using System.Collections.Generic;
+namespace PizzeriaServerTests
+{
+    internal class PizzaQueryDaoMock : IPizzaQueryDao
+    {
+        private List<Pizza> pizzaList;
+        private List<Topping> toppingList;
+
+        public PizzaQueryDaoMock()
+        {
+            pizzaList = new List<Pizza>();
+            toppingList = new List<Topping>();
+
+            Topping topping = new Topping()
+            {
+                Id = 1,
+                Name = "Topping",
+                BasePrice = 2.0m
+            };
+
+            PizzaTopping pizzaTopping = new PizzaTopping()
+            {
+                MealId = 1,
+                ToppingId = 1,
+                Topping = topping
+            };
+
+            Pizza pizza = new Pizza()
+            {
+                Id = 1,
+                Name = "Pizza",
+                Category = Category.ITALIAN,
+                BasePrice = 14.0m,
+                ActualPrice = 16.0m
+            };
+            pizzaTopping.Pizza = pizza;
+            pizza.PizzaToppings.Add(pizzaTopping);
+            pizzaList.Add(pizza);
+            toppingList.Add(topping);
+        }
+
+        public Pizza GetPizzaById(long pizzaId)
+        {
+            var result = pizzaList.Find(x => x.Id == pizzaId);
+
+            if (result == null)
+            {
+                throw new PizzaNotFoundException($"Pizza with Id: {pizzaId} not found");
+            }
+
+            return result;
+        }
+
+        public List<Pizza> GetPizzas()
+        {
+            return pizzaList;
+        }
+
+        public Topping GetToppingById(long toppingId)
+        {
+            var result = toppingList.Find(x => x.Id == toppingId);
+
+            if (result == null)
+            {
+                throw new ToppingNotFoundException($"Topping with Id: {toppingId} not found");
+            }
+
+            return result;
+        }
+
+        public List<Topping> GetToppingsForPizza(long pizzaId)
+        {
+            var pizza = GetPizzaById(pizzaId);
+
+            List<Topping> result = new List<Topping>();
+
+            foreach (PizzaTopping top in pizza.PizzaToppings)
+            {
+                result.Add(top.Topping);
+            }
+
+            return result;
+        }
+    }
+}
+
+
---
+++ PizzeriaServerTests/QueryTests/PizzeriaServerTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PizzeriaServer.Meals.Exceptions;
+using PizzeriaServer.Meals.Models;
+using PizzeriaServer.Meals.Services;
+using System;
+using System.Collections.Generic;
+
+namespace PizzeriaServerTests
+{
+    [TestClass]
+    public class PizzeriaServerTests
+    {
+        PizzaService pizzaService = new PizzaService(new PizzaQueryDaoMock());
+
+
+        [TestMethod]
+        public void get_pizza_by_choosing_correct_ID_should_pass()
+        {
+
+            var expected = pizzaService.GetPizzaById(1);
+            Assert.AreEqual(1, expected.Id);
+
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(PizzaNotFoundException))]
+        public void get_pizza_by_choosing_correct_ID_should_throw_exception()
+        {
+
+            pizzaService.GetPizzaById(2);
+
+        }
+        [TestMethod]
+        public void get_topping_by_choosing_correct_ID_should_pass()
+        {
+
+            var expected = pizzaService.GetToppingById(1);
+            Assert.AreEqual(1, expected.Id);
+
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ToppingNotFoundException))]
+        public void get_topping_by_choosing_wrong_ID_should_throw_exception()
+        {
+            pizzaService.GetToppingById(2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ToppingNotFoundException))]
+        public void get_topping_by_choosing_NaN_ID_should_throw_exception()
+
+
+        {
+            pizzaService.GetToppingById('s');
+        }
+        [TestMethod]
+        public void check_if_pizzas_are_list_should_be_list()
+        {
+            var expected = pizzaService.GetPizzas();
+            Assert.IsInstanceOfType(expected, typeof(List<Pizza>));
+        }
+
+
+        [TestMethod]
+        public void check_if_toppings_are_list_should_be_list()
+        {
+            var expected = pizzaService.GetToppingsForPizza(1);
+            Assert.IsInstanceOfType(expected, typeof(List<Topping>));
+        }
+    }
+}
